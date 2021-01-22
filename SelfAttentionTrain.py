af992445c823445fd76ea2e668f86ab9a26d052c
Frescope <1903323590@qq.com>
2021-01-22 15:10:53 +0800

    mlp test

--- SelfAttentionTrain.py
+++ SelfAttentionTrain.py
@@ -317,7 +317,7 @@ with tf.Session(config=config) as sess:
             _ = sess.run(eval_init_op)
             preds_list = []
             for eval_step in range(num_eval_batches):
-                preds = sess.run(eval_logits)  # (bc,seq_len)
+                preds = sess.run(train_logits)  # (bc,seq_len)
                 preds = preds.reshape((-1))
                 preds_list.extend(preds.tolist())
                 # batch_xs, batch_ys, batch_logits, batch_loss = sess.run(

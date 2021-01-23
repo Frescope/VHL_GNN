6441bf53e22dd098d3b1fcd8485de2a57a99e974
Frescope <1903323590@qq.com>
2021-01-23 18:21:17 +0800

    encoder test

--- SelfAttentionTrain.py
+++ SelfAttentionTrain.py
@@ -295,24 +295,28 @@ with tf.Session(config=config) as sess:
         # gradient check
         logging.info("\nStep: {} Loss: {}".format(_gs, batch_loss))
         for j in range(len(encoder_feat_observe)):
-            ftmp = encoder_feat_observe[j].reshape((10,512)).mean(axis=1)
-            logging.info(str(j)+"\t"+str(ftmp.reshape((-1))))
+            # ftmp = encoder_feat_observe[j].reshape((10,512)).mean(axis=1)
+            # logging.info(str(j)+"\t"+str(ftmp.reshape((-1))))
+            logging.info(str(enc_feat_obs[j].shape))
         for j in range(len(mlp_feat_observe)):
-            ftmp = mlp_feat_observe[j].mean(axis=-1)
-            logging.info(str(j)+"\t"+str(ftmp.reshape((-1))))
+            # ftmp = mlp_feat_observe[j].mean(axis=-1)
+            # logging.info(str(j)+"\t"+str(ftmp.reshape((-1))))
+            logging.info(str(mlp_feat_obs[j].shape))
 
         # logging.info("{} Mean, Min, Max: {:.6f} {:.6f} {:.6f}".format(
         #     str(batch_logits.shape), np.mean(batch_logits), np.min(batch_logits), np.max(batch_logits)))
         logging.info(str(batch_logits.reshape((-1))))
-        for i in range(len(grads_observe)):
-            grad, v_ = grads_observe[i]
-            var = vars_observe[i]
-            logging.info("{} (Mean, Min, Max):\t{:.6f}\t{:.6f}\t{:.6f}".format(
-                v_names[i],np.mean(var),np.min(var),np.max(var)))
-            logging.info("Gradient (Mean, Min, Max):\t{:.6f}\t{:.6f}\t{:.6f}".format(
-                np.mean(grad),np.min(grad),np.max(grad)))
+        for j in range(len(grads_observe)):
+            grad, v_ = grads_observe[j]
+            var = vars_observe[j]
+            # logging.info("{} (Mean, Min, Max):\t{:.6f}\t{:.6f}\t{:.6f}".format(
+            #     v_names[j],np.mean(var),np.min(var),np.max(var)))
+            # logging.info("Gradient (Mean, Min, Max):\t{:.6f}\t{:.6f}\t{:.6f}".format(
+            #     np.mean(grad),np.min(grad),np.max(grad)))
+            logging.info(v_names[j]+"\t"+str(v_.shape)+"\t"+str(var.shape))
         # if _gs >= 10:
         #     os._exit(0)
+        os._exit(0)
 
         if _gs and _gs % num_train_batches == 0:
             # evaluation

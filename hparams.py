66a7820a7dd85d53b8513c68b490f088887d0787
Frescope <1903323590@qq.com>
2021-01-23 13:54:44 +0800

    encoder test

--- hparams.py
+++ hparams.py
@@ -48,7 +48,7 @@ class Hparams_selfattn:
     parser.add_argument('--lr', default=0.1, type=float, help="learning rate")
     parser.add_argument('--warmup_steps', default=1000, type=int)
     parser.add_argument('--num_epochs', default=3, type=int)
-    parser.add_argument('--load_ckpt', default=True, type=bool)
+    parser.add_argument('--load_ckpt', default=False, type=bool)
 
     # checkpoint
     parser.add_argument('--ckpt_epoch', default=10000, type=int,

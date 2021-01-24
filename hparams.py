8633f6775c62bed8b68ef47a86e6f635a1c354af
Frescope <1903323590@qq.com>
2021-01-24 20:35:49 +0800

    encoder debug

--- hparams.py
+++ hparams.py
@@ -63,7 +63,7 @@ class Hparams_selfattn:
                         help="hidden dimension of feedforward layer")
     parser.add_argument('--num_blocks', default=2, type=int,
                         help="number of encoder/decoder blocks")
-    parser.add_argument('--num_heads', default=8, type=int,
+    parser.add_argument('--num_heads', default=16, type=int,
                         help="number of attention heads")
     parser.add_argument('--seq_len', default=25, type=int,
                         help="length of clip sequence")

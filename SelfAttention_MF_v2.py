9c682bd5201a686097395d7446a27a4bf86abfd6
Frescope <1903323590@qq.com>
2021-02-03 22:22:33 +0800

    encoder debug

--- SelfAttention_MF_v2.py
+++ SelfAttention_MF_v2.py
@@ -518,8 +518,8 @@ def run_training(data_train, data_test, test_mode):
             audio_biases = {
                 'bc5': _variable_with_weight_decay('au_bc5', [256], 0.0000),
             }
-        varlist_visual = list(biases.values())
-        varlist_audio = list(audio_biases.values())
+        varlist_visual = list(weights.values()) + list(biases.values())
+        varlist_audio = list(audio_weights.values()) + list(audio_biases.values())
         # training operations
         lr = noam_scheme(LR_TRAIN,global_step,WARMUP_STEP)
         opt_train = tf.train.AdamOptimizer(lr)
@@ -546,7 +546,8 @@ def run_training(data_train, data_test, test_mode):
                 loss_name_scope = ('gpud_%d_loss' % gpu_index)
                 loss = tower_loss(loss_name_scope, logits, labels)
                 varlist = tf.trainable_variables()  # 全部训练
-                varlist = list(set(varlist) - set(varlist_visual) - set(varlist_audio))
+                # varlist = list(set(varlist) - set(varlist_visual) - set(varlist_audio))
+                varlist = varlist + list(biases.values()) + list(audio_biases.values())
                 grads_train = opt_train.compute_gradients(loss, varlist)
                 thresh = GRAD_THRESHOLD  # 梯度截断 防止爆炸
                 grads_train_cap = [(tf.clip_by_value(grad, -thresh, thresh), var) for grad, var in grads_train]

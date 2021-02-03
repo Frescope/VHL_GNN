1abe565ceb2ba718374c234085d0dffa7146b03b
Frescope <1903323590@qq.com>
2021-02-03 20:31:13 +0800

    encoder debug

--- SelfAttention_MF_v2.py
+++ SelfAttention_MF_v2.py
@@ -609,9 +609,13 @@ def run_training(data_train, data_test, test_mode):
                 ob_loss.clear()
                 logging.info(' Step %d: %.3f sec' % (step, duration))
                 logging.info(' Evaluate: '+str(step)+' Epoch: '+str(epoch))
+                logging.info('\n')
                 logging.info(str(np.mean(loss_array)))
+                logging.info('\n')
                 logging.info(str(np.min(loss_array)))
+                logging.info('\n')
                 logging.info(str(np.max(loss_array)))
+                logging.info('\n')
                 logging.info(' Average Loss: '+str(np.mean(loss_array))+' Min Loss: '+str(np.min(loss_array))+' Max Loss: '+str(np.max(loss_array)))
 
                 # 按顺序预测测试集中每个视频的每个分段，全部预测后在每个视频内部排序，计算指标

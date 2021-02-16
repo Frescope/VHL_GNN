4d8823e3861ca2cb31d59b1b3c92e5bc6e030528
Frescope <1903323590@qq.com>
2021-02-16 12:19:48 +0800

    bilibili-test

--- SelfAttention_MF_v2.py
+++ SelfAttention_MF_v2.py
@@ -18,7 +18,7 @@ import argparse
 import Transformer
 from Transformer import self_attention
 
-SERVER = 1
+SERVER = 0
 
 class Path:
     parser = argparse.ArgumentParser()
@@ -669,17 +669,17 @@ def run_training(data_train, data_test, test_mode):
                     if f > max_f1:
                         max_f1 = f
                     model_path = model_save_dir + 'S%d-E%d-L%.6f-F%.3f' % (step,epoch,np.mean(loss_array),f)
-                    saver_overall.save(sess, model_path)
+                    # saver_overall.save(sess, model_path)
                     logging.info('Model Saved: '+model_path+'\n')
 
             if step % 5000 == 0 and step > 0:
                 model_path = model_save_dir + 'S%d-E%d' % (step+PRESTEPS, epoch)
-                saver_overall.save(sess, model_path)
+                # saver_overall.save(sess, model_path)
                 logging.info('Model Saved: '+str(step + PRESTEPS))
 
             # saving final model
         model_path = model_save_dir + 'S%d' % (MAXSTEPS + PRESTEPS)
-        saver_overall.save(sess, model_path)
+        # saver_overall.save(sess, model_path)
         logging.info('Model Saved: '+str(MAXSTEPS + PRESTEPS))
 
     return
--- Transformer.py
+++ Transformer.py
@@ -6,7 +6,7 @@ D_MODEL = 512
 D_FF = 2048
 SEQ_LEN = 10
 INTERVAL = 1
-NUM_BLOCKS = 2
+NUM_BLOCKS = 6
 NUM_HEADS = 32
 
 def positional_encoding(inputs, scope='positional_encoding'):
